0998f60d2b849f6a8af2bd6b0a900d0d367b8847
Krls-Pan <81246479+Krls-Pan@users.noreply.github.com>
2021-05-11 16:18:14 +0200

    Update secexp.py

--- src/secexp.py
+++ src/secexp.py
@@ -58,3 +58,51 @@ def write_page_FR():
         components.html(source_code1, height = 600)
         st.markdown(s12, unsafe_allow_html=True)
         st.markdown(s121, unsafe_allow_html=True)
+
+
+
+def write_page_ENG():
+    s1 = """
+        <div style="text-align: center; font-size: 25px"> <br><br><b> General analysis of the number and distances of interventions <br><br>
+        </div>
+        """
+    s11  = """
+        <div style="text-align: center; font-size: 20px"> 
+        <b>Number of interventions per stations
+        </div>
+        """
+    s111 = """
+        <div style=" font-size: 15px">  
+        We can observe that the stations in the center of the London urban area stand out strongly from the stations in the suburbs. The network of <b> central stations </b> also appears to be more <b> concentrated </b> in this area, in line with its <b> urban density </b>. The further away from the center, the more the intervention rate decreases. <br>
+        Again, it is very likely that the main explanation lies in the variation in urban density. The stations of <b> Soho </b>, with more than <b> 58,000 </b> interventions, and of <b> Paddington </b>, more than <b> 42,000 </b>, peculiarly stand out while, for comparison, the outlying stations of <b> Wennington </b> or <b> Biggin Hill </b> intervened on average around <b> 5,000 </b> times.  
+        </div>
+        """
+    s12  = """
+        <div style="text-align: center; font-size: 20px"> 
+        <b>Average distances of interventions per stations
+        </div>
+        """ 
+    s121 = """
+        <div style=" font-size: 15px"> 
+        The variation in the station network density can also be observed with the study of the average distance traveled by stations. There too a difference between the center and the outskirts can be observed: The <b> center </b> stations in fact cover <b> less distance </b> on average than the <b> stations in the peripheries </ b >.
+        <br> On average, the <b> center </b> stations cover approximately <b> 2.2km </b> in their interventions, while the <b> periphery </b> stations cover approximately <b> 4km </b>.
+        </div>
+        """
+    
+
+
+    
+    st.markdown(s1, unsafe_allow_html=True)
+    cols2 = st.beta_columns(2)
+    with cols2[0]:
+        Htmlinter = open("figures/inter.html", 'r', encoding='utf-8')
+        source_code = Htmlinter.read()
+        components.html(source_code, height = 600)
+        st.markdown(s11, unsafe_allow_html=True)
+        st.markdown(s111, unsafe_allow_html=True)
+    with cols2[1]:
+        Htmldist = open("figures/distmean.html", 'r', encoding='utf-8')
+        source_code1 = Htmldist.read()
+        components.html(source_code1, height = 600)
+        st.markdown(s12, unsafe_allow_html=True)
+        st.markdown(s121, unsafe_allow_html=True)
