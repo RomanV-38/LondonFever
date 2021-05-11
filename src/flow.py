ebb207e094c90b10997aec052b9ead330d67e6f1
Krls-Pan <81246479+Krls-Pan@users.noreply.github.com>
2021-05-11 16:17:55 +0200

    Update flow.py

--- src/flow.py
+++ src/flow.py
@@ -31,4 +31,36 @@ def write_page_FR():
         st.image('figures/flowchart.png')
     with cols[1]:
         st.markdown(description, unsafe_allow_html=True)
+        
+        
+def write_page_ENG():
+    s_t = """
+    <div style="text-align: center; font-size: 25px"> <b> Illustrating the final application procedure (Prediction Tool tab)
+    </div>
+    """
+    description = '''
+    <br><br><br><br><br><br>
+    <div style="font: 15px"> <b> 
+    Once the Dataset is loaded </b>, we can select new coordinates (on the map).
+    <br> <br> <br> <br> <br>
+    <b> We use the pre-trained Gaussian Mixture Model  (GMM)</b>, to determine the
+    probability that this incident is attributed to each of the stations.
+    <br> <br> <br> <br>
+    Then, <b> we set a threshold </b>: in our case, it is 0.1%.
+    With this threshold, one of the stations selected matched the intervening station in the Dataset in 95% of cases.
+    <br><br><br><br>
     
+    <b> For each of the selected stations </b>, we calculate the Euclidean distance between the station and the incident.
+    We observed that for our Dataset, <b> the distance traveled is about 75% of the Euclidean distance </b>
+    calculated on UTM coordinates.
+    
+    <br>
+    We can then <b> predict the response time </b>, using pre-trained linear regressions for each station.   
+    </div>
+    '''
+    st.markdown(s_t, unsafe_allow_html=True)
+    cols = st.beta_columns(2)
+    with cols[0]:
+        st.image('figures/flowchart.png')
+    with cols[1]:
+        st.markdown(description, unsafe_allow_html=True)
